74570d4fcd5ba5d7dfd62875440dfdd1b35c957d
miakojason <miakojason@gmail.com>
2024-02-16 11:31:02 +0800

    j完成首頁顯示商品分類顯示

--- index.php
+++ index.php
@@ -53,19 +53,19 @@
                 </div>
                 <div id="left" class="ct">
                         <div style="min-height:400px;">
-                                <a>全部商品</a>
+                                <a href="?type=0">全部商品(<?=$Goods->count(['sh'=>1]);?>)</a>
                                 <?php
                                 $bigs = $Type->all(['big_id' => 0]);
                                 foreach ($bigs as $big) {
                                 ?>
-                                        <div class="ww"><a href=""><?= $big['name']; ?></a>
+                                        <div class="ww"><a href="?type=<?=$big['id'];?>"><?= $big['name'];?>(<?=$Goods->count(['sh'=>1,'big'=>$big['id']]);?>)</a>
                                                 <div class="s">
                                                         <?php
                                                         if ($Type->count(["big_id" => $big['id']]) > 0) {
                                                                 $mids = $Type->all(["big_id" => $big['id']]);
                                                                 foreach($mids as $mid){
                                                                         ?>
-                                                                <a href=""><?=$mid['name'];?></a>
+                                                                <a href="?type=<?=$mid['id'];?>"><?=$mid['name'];?>(<?=$Goods->count(['sh'=>1,'mid'=>$mid['id']]);?>)</a>
                                                                         <?php
                                                                 }
                                                         }
