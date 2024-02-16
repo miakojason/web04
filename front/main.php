a366ad51746e99e14397da94d965a3c96d443a68
miakojason <miakojason@gmail.com>
2024-02-16 11:42:03 +0800

    首頁取得不同的商品資料

--- front/main.php
+++ front/main.php
@@ -1,17 +1,26 @@
 <?php
 $type=$_GET['type']??0;
 $nav='';
-
+$goods='';
 if($type==0){
-$nav="全部商品";
+    $nav="全部商品";
+    $goods=$Goods->all(['sh'=>1]);
 }else{
     $t=$Type->find($type);
     if($t['big_id']==0){
         $nav=$t['name'];
+        $goods=$Goods->all(['sh'=>1,'big'=>$t['id']]);
     }else{
         $b=$Type->find($t['big_id']);
         $nav=$b['name'] . " > " . $t['name'];
+        $goods=$Goods->all(['sh'=>1,'mid'=>$t['id']]);
     }
 }
 ?>
 <h2><?=$nav;?></h2>
+<?php
+foreach($goods as $good){
+    echo $good['name'];
+    echo "<br>";
+}
+?>
